c4bb7eade24b0386ac08dbc9342a6683e72ccf94
kiki-2002-11 <134251104+kiki-2002-11@users.noreply.github.com>
2023-10-16 22:56:36 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,13 @@
+def add_items(n):
+    return n + n + n
+
+print (add_items(10))
+
+def print_items(a,b):
+    for i in range(a):
+        print(i)
+
+    for j in range(b):
+        print(j)
+
+print_items(1, 10)
